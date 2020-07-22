dd631239a082f5e9e8bd0d00af42f1c979979f42
Tia Krofel <tia.krofel@gmail.com>
2020-07-22 19:45:07 +0200

    Spremenjeni podatki v testu

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ knjigozer = Knjigozer()
 
 neprebrana = knjigozer.dodaj_neprebrano('Donna Tartt', 'The Goldfinch', 'DA')
 trenutna = knjigozer.dodaj_trenutno(neprebrana, 110)
-dokoncana = knjigozer.dokoncana(date(2019, 8, 20), trenutna, 771, '9')
+dokoncana = knjigozer.dokoncana(date(2019, 8, 20), trenutna, 771, '9') 
 
 print(knjigozer)
