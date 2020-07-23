0a09f43ab39a483f96c1bf259d40479bd0e8051d
Tia Krofel <tia.krofel@gmail.com>
2020-07-23 11:44:23 +0200

    Novi podatki za testiranje modela

--- test.py
+++ test.py
@@ -4,7 +4,14 @@ from model import Knjigozer
 knjigozer = Knjigozer()
 
 neprebrana = knjigozer.dodaj_neprebrano('Donna Tartt', 'The Goldfinch', 'DA')
-trenutna = knjigozer.dodaj_trenutno(neprebrana, 110)
-dokoncana = knjigozer.dokoncana(date(2019, 8, 20), trenutna, 771, '9') 
+trenutna = knjigozer.izberi_trenutno(neprebrana, 771)
+dokoncana = knjigozer.dokoncana(date(2019, 8, 20), trenutna, '7/7') 
+
+neprebrana1 = knjigozer.dodaj_neprebrano('Neil Gaiman', 'The Graveyard Book', "Children's Books")
+
+trenutna1 = knjigozer.dodaj_trenutno('Stephen Fry', 'Mythos', 'myths', 416, 82)
+
+dokoncana1 = knjigozer.dodaj_prebrano(date(2019, 7, 1), 'Madeline Miller', 'Circe', 'historical fiction', 393, '6/7')
+
 
 print(knjigozer)
