4c0ff0a674e2e52d2297c5e6e338d9acbc675bba
Tia Krofel <tia.krofel@gmail.com>
2020-07-23 19:45:47 +0200

    Posodobljen test

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ from model import Knjigozer
 
 knjigozer = Knjigozer()
 
-neprebrana = knjigozer.dodaj_neprebrano('Donna Tartt', 'The Goldfinch', 'DA')
-trenutna = knjigozer.izberi_trenutno(neprebrana, 771)
-dokoncana = knjigozer.dokoncana(date(2019, 8, 20), trenutna, '7/7') 
+neprebrana1 = knjigozer.dodaj_neprebrano('Donna Tartt', 'The Goldfinch')
+trenutna1 = knjigozer.izberi_trenutno(neprebrana1, 771)
+dokoncana1 = knjigozer.dokoncana(date(2019, 8, 20), trenutna1, '7/7') 
 
-neprebrana1 = knjigozer.dodaj_neprebrano('Neil Gaiman', 'The Graveyard Book', "Children's Books")
+neprebrana2 = knjigozer.dodaj_neprebrano('Neil Gaiman', 'The Graveyard Book')
 
-trenutna1 = knjigozer.dodaj_trenutno('Stephen Fry', 'Mythos', 'myths', 416, 82)
+trenutna2 = knjigozer.dodaj_trenutno('Stephen Fry', 'Mythos', 416, 82)
 
-dokoncana1 = knjigozer.dodaj_prebrano(date(2019, 7, 1), 'Madeline Miller', 'Circe', 'historical fiction', 393, '6/7')
+dokoncana2 = knjigozer.dodaj_prebrano(date(2019, 7, 1), 'Madeline Miller', 'Circe', 393, '6/7')
 
 
 print(knjigozer)
