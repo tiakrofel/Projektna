a9a7dd0e93f42813745fba1d037be22570056f3d
Tia Krofel <tia.krofel@gmail.com>
2020-07-27 10:30:14 +0200

    Popravljen zapis avtorjev

--- test.py
+++ test.py
@@ -4,15 +4,15 @@ from model import Knjigozer
 
 knjigozer = Knjigozer()
 
-neprebrana1 = knjigozer.dodaj_neprebrano('Donna Tartt', 'The Goldfinch')
+neprebrana1 = knjigozer.dodaj_neprebrano('Tartt, Donna', 'The Goldfinch')
 trenutna1 = knjigozer.izberi_trenutno(neprebrana1, 771)
 dokoncana1 = knjigozer.dokoncana(date(2019, 8, 20), trenutna1, '7/7') 
 
-neprebrana2 = knjigozer.dodaj_neprebrano('Neil Gaiman', 'The Graveyard Book')
+neprebrana2 = knjigozer.dodaj_neprebrano('Gaiman, Neil', 'The Graveyard Book')
 
-trenutna2 = knjigozer.dodaj_trenutno('Stephen Fry', 'Mythos', 416, 82)
+trenutna2 = knjigozer.dodaj_trenutno('Fry, Stephen', 'Mythos', 416, 82)
 
-dokoncana2 = knjigozer.dodaj_prebrano(date(2019, 7, 1), 'Madeline Miller', 'Circe', 393, '6/7')
+dokoncana2 = knjigozer.dodaj_prebrano(date(2019, 7, 1), 'Miller, Madeline', 'Circe', 393, '6/7')
 
 knjigozer.nova_kategorija('Moje najljubše knjige')
 knjigozer.v_kategorijo('Moje najljubše knjige', dokoncana1)
